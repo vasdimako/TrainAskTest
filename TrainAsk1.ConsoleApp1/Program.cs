5998ad7ea6ef4aec669db1771ffd9b1943186660
vasdimako <38927700+vasdimako@users.noreply.github.com>
2022-03-19 22:11:04 +0200

    Split solutions.

--- TrainAsk1.ConsoleApp1/Program.cs
+++ TrainAsk1.ConsoleApp1/Program.cs
@@ -1,42 +1,59 @@
-﻿using System;
+﻿int seqCounter = 0;
+int entityCounter = 0;
 
-namespace TrainAsk1.ConsoleApp1;
+// Take console inputs for the directories.
 
-class Program
+Console.WriteLine("Please enter input file path:");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string inputpath = Console.ReadLine();
+Console.WriteLine("Please enter output file name:");
+string outputpath = Console.ReadLine();
+outputpath = Directory.GetParent(inputpath) + @"\" + "output.txt";
+
+// Split sequences into separate files.
+Console.WriteLine("Split sequences into separate files? Y/N");
+string splitQ = Console.ReadLine();
+
+if (splitQ == "Y")
 {
-    static void Main(string[] args)
-    {
-        int seqCounter = 0;
-        int entityCounter = 0;
-
-        using StreamWriter file = new(path: @"C:\test");
-        // Read the file and display it line by line.  
-        foreach (string line in System.IO.File.ReadLines(@"c:\test.txt"))
-        {
-            // System.Console.WriteLine(line);
-            if (line.StartsWith(">"))
-            {
-                if (entityCounter > 0)
-                {
-                    System.Console.WriteLine(""); // WriteLine adds a newline character to the end of whatever is written
-                    file.WriteLine("");
-                }
-                // System.Console.Write($"{Environment.NewLine}"); // Write a newline character, appropriate depending on the environment
-                entityCounter++;
-                System.Console.WriteLine(line);
-                file.WriteLine(line);
-            }
-                
-            else
-                System.Console.Write(line); // Write just writes what we specified without a newline character.
-                file.WriteLine(line);
-                seqCounter++;
-        }
-        file.Close();
-        System.Console.WriteLine("There were {0} entities.", entityCounter);
-        System.Console.WriteLine("There were {0} sequences.", seqCounter);
-        // Suspend the screen.  
-        System.Console.ReadLine(); // This makes sure the console waits for our Enter input before the text disappears.
-    }
+    file.Close();
+    using StreamWriter file = new(path: Directory.GetParent(inputpath) + @"\" + "output{0}.txt", entityCounter);
+    // Also have to change the entityCounter > 0 if statement to something that looks at if this is the first file line instead.
 }
 
+// To set the number of allowable columns per line, we might need to go character-by-character rather than line by line when we write.
+// Or maybe we write it in one line and then go through that line and add newline characters.
+
+inputpath = @"C:\testpath\test.fasta";
+outputpath = @"C:\testpath\output.txt";
+
+using StreamWriter file = new(path: outputpath);
+// Read the file and display it line by line.
+foreach (string line in System.IO.File.ReadLines(inputpath))
+{
+    // System.Console.WriteLine(line);
+    if (line.StartsWith(">"))
+    {
+        if (entityCounter > 0)
+        {
+            Console.WriteLine(""); // WriteLine adds a newline character to the end of whatever is written
+            file.WriteLine("");
+        }
+
+        // System.Console.Write($"{Environment.NewLine}"); // Write a newline character, appropriate depending on the environment
+        entityCounter++;
+        System.Console.WriteLine(line);
+        file.WriteLine(line);
+    }
+
+    else
+    file.Write(line);
+    seqCounter++;
+}
+file.Close();
+
+System.Console.WriteLine("There were {0} entities.", entityCounter);
+System.Console.WriteLine("There were {0} sequences.", seqCounter);
+// Suspend the screen.
+System.Console.ReadLine(); // This makes sure the console waits for our Enter input before the text disappears.
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
